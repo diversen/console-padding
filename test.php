e3d2103b69f75dd79dfbfcbe0af4544f80e20979
Dennis Iversen <dennis.iversen@gmail.com>
2016-12-12 20:53:14 +0100

    auto commit

--- test.php
+++ test.php
@@ -15,7 +15,22 @@ $ary = array(
 // Default
 echo "Default settings:\n";
 echo $p->padArray($ary);
-
+echo "\n";
 echo "User defined settings:\n";
 // Change init of row, between row, and end of row
 echo $p->padArray($ary, $begin_row = ' | ', $between_row = ' | ', $rowend = "\n");
+echo "\n";
+
+/*
+Yields: 
+  Default settings: 
+
+    1. column 1. ro  some details          andsome more details  0  
+    1. column 2. ro  andsome more details  
+
+  User defined settings:
+
+   | 1. column 1. ro | some details         | andsome more details | 0 | 
+   | 1. column 2. ro | andsome more details | 
+ * 
+ */
